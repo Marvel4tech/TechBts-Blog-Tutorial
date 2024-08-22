3ddb1ccd3aa40b74b882faec39e08c1e2c218230
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-08-23 00:16:29 +0100

    commit

--- src/components/Home/Write/Preview.jsx
+++ src/components/Home/Write/Preview.jsx
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from 'react'
 import { LiaTimesSolid } from 'react-icons/lia'
+import ReactQuill from 'react-quill'
 
 const Preview = () => {
     const imageRef = useRef(null)
@@ -19,9 +20,15 @@ const Preview = () => {
                     <h3>Story Preview</h3>
                     <div className=' w-full h-[200px] object-cover bg-gray-200 my-3 grid place-items-center cursor-pointer bg-cover 
                     bg-no-repeat' onClick={handleClick} style={{backgroundImage : `url(${imageUrl})`}}>
-                        Add Image
+                        {!imageUrl && `Add Image`}
                     </div>
                     <input onChange={(e) => setImageUrl(URL.createObjectURL(e.target.files[0]))} ref={imageRef} type="file" hidden />
+                    <input type="text" placeholder='Title' className=' outline-none w-full border-b border-gray-300 py-2' />
+                    <ReactQuill theme='bubble' placeholder='Tell your story...' className=' py-3 border-b border-gray-300' />
+                    <p className=' text-gray-500 pt-4 text-sm'>
+                        <span className=' font-bold'>Note:</span> Changes here will affect how your story appears in public places like
+                        TechBTS's homepage and in subscribers' inboxes - not the contents of the story itself
+                    </p>
                 </div>
             </div>
         </div>
