d856980a1a19eb44d237244b2428569ca74b6faa
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-08-22 23:41:36 +0100

    commit

--- src/components/Home/Write/Write.jsx
+++ src/components/Home/Write/Write.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import ReactQuill from 'react-quill'
+import Preview from './Preview'
 
 const Write = () => {
     const [value, setValue] = useState("")
@@ -8,6 +9,9 @@ const Write = () => {
     <section className=' w-[90%] md:w-[90%] lg:w-[60%] py-[3rem] mx-auto'>
         <input type="text" placeholder='Title' className=' text-4xl w-full outline-none' />
         <ReactQuill theme='bubble' value={value} onChange={setValue} placeholder='Tell your story...' className=' write py-5' />
+        <div>
+            <Preview />
+        </div>
     </section>
   )
 }
