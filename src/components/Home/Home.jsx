181873253be2a43f03c9edd3b216364c8378162b
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-08-23 15:12:12 +0100

    commit

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,17 @@
 import { Routes, Route } from "react-router-dom"
+import Posts from "../common/Posts/Posts"
 
 
 const Home = () => {
   return (
-    <main>
-        Home
-    </main>
+    <section className=" size flex relative gap-[5rem]">
+        <div className=" flex-[2] py-10 mb-[4rem]">
+            <Posts />
+        </div>
+        <div className=" hidden md:inline-block md:w-[21rem] p-7 border-1 border-gray-300">
+            <h3>Who to follow?</h3>
+        </div>
+    </section>
   )
 }
 
