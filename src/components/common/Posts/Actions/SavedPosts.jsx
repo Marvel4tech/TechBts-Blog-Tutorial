b3724bb604da73ac64e75e082ba83947e12b80c9
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-08-24 16:16:17 +0100

    commit

--- src/components/common/Posts/Actions/SavedPosts.jsx
+++ src/components/common/Posts/Actions/SavedPosts.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { CiSaveDown2 } from 'react-icons/ci'
+import { Blog } from '../../../../context/Context'
+import { doc } from '@firebase/firestore'
 
 const SavedPosts = ({ post }) => {
+    const [isSaved, setIsSaved] = useState(false)
+    const { currentUser } = Blog()
+
+    const handleSave = async () => {
+        try {
+            if(currentUser) {
+                const saveRef = doc(db, "users", currentUser?.uid, "savePost", post?.id)
+            }
+        } catch (error) {
+            
+        }
+    }
+
   return (
     <>
-        <button className=' hover:opacity-60'>
-            <CiSaveDown2 className=' text-2xl pointer-events-none' />
+        <button onClick={handleSave} className=' hover:opacity-60'>
+            <CiSaveDown2 className={`text-2xl pointer-events-none ${isSaved ? "text-yellow-600" : ""}`} />
         </button>
     </>
   )
