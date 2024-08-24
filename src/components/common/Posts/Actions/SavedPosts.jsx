bc5d23bad2ea385f4352a9ea15259dd6a63908dc
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-08-24 17:03:10 +0100

    commit

--- src/components/common/Posts/Actions/SavedPosts.jsx
+++ src/components/common/Posts/Actions/SavedPosts.jsx
@@ -1,13 +1,19 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { CiSaveDown2 } from 'react-icons/ci'
 import { Blog } from '../../../../context/Context'
 import { deleteDoc, doc, setDoc } from '@firebase/firestore'
 import { toast } from 'react-toastify'
 import { db } from '../../../../firebaseConfig/firebase'
+import useSingleFetch from '../../../hook/useSingleFetch'
 
 const SavedPosts = ({ post }) => {
     const [isSaved, setIsSaved] = useState(false)
     const { currentUser } = Blog()
+    const { data, loading } = useSingleFetch("users", currentUser?.uid, "savePost")
+
+    useEffect(() => {
+        setIsSaved(data && data.find((item)  => item.id === post.id ))
+    }, [data, post?.id])
 
     const handleSave = async () => {
         try {
@@ -21,7 +27,7 @@ const SavedPosts = ({ post }) => {
                     await setDoc(saveRef, {
                         ...post,
                     })
-                    toast.success("Post has been Save")
+                    toast.success("Post has been Saved")
                 }
             }
         } catch (error) {
