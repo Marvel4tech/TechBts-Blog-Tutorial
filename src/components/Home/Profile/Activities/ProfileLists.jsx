2613e416a342905330d6d003c07a810ab0034fd9
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-08-26 13:25:27 +0100

    ProfileList Save posts is added

--- src/components/Home/Profile/Activities/ProfileLists.jsx
+++ src/components/Home/Profile/Activities/ProfileLists.jsx
@@ -3,6 +3,7 @@ import useSingleFetch from '../../../hook/useSingleFetch'
 import { Blog } from '../../../../context/Context'
 import Loading from '../../../Loading/Loading'
 import PostCard from '../../../common/Posts/PostCard'
+import { BiLock } from 'react-icons/bi'
 
 const ProfileLists = ({ getUserData }) => {
   const { currentUser } = Blog()
@@ -10,7 +11,7 @@ const ProfileLists = ({ getUserData }) => {
 
   return (
       <div>
-         {currentUser?.uid === getUserData?.userId ? (
+         {currentUser?.uid !== getUserData?.userId ? (
            <div className=' flex flex-col gap-[2rem] mb-[2rem]'>
               {data.length === 0 && (
                 <p className=' text-gray-500'>
@@ -32,6 +33,9 @@ const PrivateLists = ({ username }) => {
         <p>
             <span className=' capitalize'>{username} saved posts are private</span>
         </p>
+        <span className=' text-[10rem] text-gray-500'>
+            <BiLock />
+        </span>
     </div>
   )
 }
