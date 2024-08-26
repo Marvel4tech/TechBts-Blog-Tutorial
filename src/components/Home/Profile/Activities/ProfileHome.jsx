f41235d042e34ccb593e124a7726ce222cb2f673
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-08-26 16:13:46 +0100

    commit

--- src/components/Home/Profile/Activities/ProfileHome.jsx
+++ src/components/Home/Profile/Activities/ProfileHome.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import useFetch from '../../../hook/useFetch'
+import Loading from '../../../Loading/Loading'
+import PostCard from '../../../common/Posts/PostCard'
 
 const ProfileHome = ({ getUserData }) => {
-  const [data, loading] = useFetch("posts")
+  const { data, loading } = useFetch("posts")
   const userPost = data && data?.filter((post) => post?.userId === getUserData?.userId)
 
   return (
     <div className=' flex flex-col gap-5 mb-[4rem]'>
-        {userPost.length === 0 && <p></p>}
+        {userPost.length === 0 && 
+          <p className=' text-gray-500'>
+            <span className=' capitalize'>{getUserData?.username}</span> has no post
+          </p>
+        }
+        {loading ? (
+          <Loading/>
+        ) : (
+          userPost.map((post, i) => <PostCard post={post} key={i} />)
+        )}
     </div>
   )
 }
