0a2e014525b0801ff1fb608a17ac81347c30df6a
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-08-26 16:00:15 +0100

    commit

--- src/components/Home/Profile/Activities/ProfileHome.jsx
+++ src/components/Home/Profile/Activities/ProfileHome.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 import useFetch from '../../../hook/useFetch'
 
 const ProfileHome = ({ getUserData }) => {
+  const [data, loading] = useFetch("posts")
+  const userPost = data && data?.filter((post) => post?.userId === getUserData?.userId)
+
   return (
-    const [data, loading] = useFetch()
+    <div className=' flex flex-col gap-5 mb-[4rem]'>
+        {userPost.length === 0 && <p></p>}
+    </div>
   )
 }
 
