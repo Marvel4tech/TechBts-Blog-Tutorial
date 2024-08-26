bdd4d96f7cdd9bd887c2f28855809205adcf7e35
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-08-26 12:49:03 +0100

    commit

--- src/components/Home/Profile/Activities/ProfileLists.jsx
+++ src/components/Home/Profile/Activities/ProfileLists.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import useSingleFetch from '../../../hook/useSingleFetch'
 
 const ProfileLists = () => {
+  const { data, loading } = useSingleFetch("users", currentUser?.uid, "savePost")
+
   return (
     <div>ProfileLists</div>
   )
