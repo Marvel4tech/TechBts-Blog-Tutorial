05eae235fee895ce120578aa63d5a23047fd8f00
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-08-27 16:38:12 +0100

    commit

--- src/components/Home/UserToFollow/Follow.jsx
+++ src/components/Home/UserToFollow/Follow.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { useState } from "react"
+import useFetch from "../../hook/useFetch"
+import { Blog } from "../../../context/Context"
+
 
 const Follow = () => {
+    const { data, loading } = useFetch("users")
+    const { currentUser } = Blog()
+    const [count, setCount] = useState(5)
+
+    const users = data && data?.slice(0, count).filter((user) => user.userId !== currentUser?.uid)
+
   return (
-    <div>Follow</div>
+    <div>
+
+    </div>
   )
 }
 
