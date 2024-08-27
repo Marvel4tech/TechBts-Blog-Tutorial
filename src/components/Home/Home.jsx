19bc085d2a25a8bf883fb34ee42dfd33b596eeb6
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-08-27 17:25:40 +0100

    commit

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -9,8 +9,8 @@ const Home = () => {
         <div className=" flex-[2] py-10 mb-[4rem]">
             <Posts />
         </div>
-        <div className=" hidden md:inline-block md:w-[21rem] p-7 border-1 border-gray-300">
-            <h3>Who to follow?</h3>
+        <div className=" hidden md:inline-block md:w-[21rem] p-7 border-l border-gray-300">
+            <h3 className=" font-bold">Who to follow?</h3>
             <Follow />
         </div>
     </section>
