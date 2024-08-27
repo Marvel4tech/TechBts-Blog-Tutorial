084a8b885b6188f431ce3b13e98606ea4a19cd0c
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-08-27 15:35:35 +0100

    commit

--- src/utilities/DropDown.jsx
+++ src/utilities/DropDown.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 const DropDown = ({ children, size, showDrop, setShowDrop }) => {
+    const dropRef = useRef(null)
+
+    useEffect(() => {
+        const clickOutside = (e) => {
+            if (!dropRef.current.contains(e.target)){
+                setShowDrop(false)
+            }
+        }
+        window.addEventListener("mousedown", clickOutside);
+        return () => window.removeEventListener("mousedown", clickOutside);
+    }, [])
+
   return (
     <>
         { showDrop && (
-            <div className={` shadow-2xl flex flex-col absolute right-0 top-[2rem] bg-white border ${size}`}>
+            <div ref={dropRef} className={` shadow-2xl flex flex-col absolute right-0 top-[2rem] bg-white border ${size}`}>
                 {children}
             </div>
         )}
