6e0cf3f54b29d5b7041c4fb5cada6d80801cf697
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-08-29 06:56:59 +0100

    commit

--- src/components/common/Posts/SinglePost.jsx
+++ src/components/common/Posts/SinglePost.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import { doc, getDoc } from '@firebase/firestore'
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { db } from '../../../firebaseConfig/firebase'
 
 const SinglePost = () => {
+    const { postId } = useParams()
+
+    useEffect(() => {
+        const fetchPost = async () => {
+            try {
+                const postRef = doc(db, "posts", postId)
+                const getPost = getDoc(postRef)
+            } catch (error) {
+                
+            }
+        }
+    }, [])
   return (
     <div>SinglePost</div>
   )
