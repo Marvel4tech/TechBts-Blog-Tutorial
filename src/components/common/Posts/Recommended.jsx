8c5e572b27aefc0e7281d23c52d4c6976829aa74
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-08-31 03:19:04 +0100

    commit

--- src/components/common/Posts/Recommended.jsx
+++ src/components/common/Posts/Recommended.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import { useEffect } from "react"
+import useFetch from "../../hook/useFetch"
+
 
 const Recommended = () => {
+  const {data} = useFetch("posts")
+
+  let recommendedPost = [];
+
+  useEffect(() => {
+    
+  }, [])
+
   return (
     <div>Recommended</div>
   )
