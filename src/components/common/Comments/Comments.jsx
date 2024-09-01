495e8d229f632bcdb260ae7e6790c6394788ac14
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-09-01 12:54:51 +0100

    beginning of adding comment section

--- src/components/common/Comments/Comments.jsx
+++ src/components/common/Comments/Comments.jsx
@@ -8,7 +8,7 @@ const Comments = () => {
     <Modal setModal={showModal} modal={showModal}>
         <section className={` fixed right-0 bottom-0 top-0 border border-gray-300 z-50 bg-white w-[22rem] shadow-2xl p-5 overflow-y-auto transition-all 
         duration-500`}>
-            <div>
+            <div className=" flex items-center justify-between">
                 <h3>
                     Responses(1)
                 </h3>
