64b7820ffe62342b8052b8091ed1cdc1db4fb162
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-09-01 01:10:44 +0100

    beginning of adding Like functionality

--- src/components/common/Posts/Actions/Like.jsx
+++ src/components/common/Posts/Actions/Like.jsx
@@ -1,6 +1,17 @@
 import { PiHandsClappingDuotone } from 'react-icons/pi'
+import { Blog } from '../../../../context/Context'
 
-const Like = () => {
+const Like = ({ post }) => {
+  const { currentUser } = Blog()
+
+  const handleLike = async () => {
+    try {
+      
+    } catch (error) {
+      
+    }
+  }
+
   return (
     <button className=' flex items-center gap-1 text-sm'>
         <PiHandsClappingDuotone className=' text-xl' />
