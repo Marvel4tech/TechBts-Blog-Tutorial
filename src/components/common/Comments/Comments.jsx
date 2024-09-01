dc3a3674bf17e0c6a03c0cd92fd091b56d4da8d2
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-09-01 13:01:43 +0100

    commit

--- src/components/common/Comments/Comments.jsx
+++ src/components/common/Comments/Comments.jsx
@@ -1,17 +1,21 @@
 import { useState } from "react"
 import Modal from "../../../utilities/Modal"
+import { LiaTimesSolid } from 'react-icons/lia'
 
 const Comments = () => {
     const [showModal, setShowModal] = useState(true)
 
   return (
-    <Modal setModal={showModal} modal={showModal}>
+    <Modal setModal={setShowModal} modal={showModal}>
         <section className={` fixed right-0 bottom-0 top-0 border border-gray-300 z-50 bg-white w-[22rem] shadow-2xl p-5 overflow-y-auto transition-all 
-        duration-500`}>
+        duration-500 ${showModal ? "translate-x-0" : "translate-x-[23rem]"}`}>
             <div className=" flex items-center justify-between">
-                <h3>
+                <h3 className=" text-xl font-bold">
                     Responses(1)
                 </h3>
+                <button onClick={() => setShowModal(false)} className=" text-xl">
+                    <LiaTimesSolid />
+                </button>
             </div>
         </section>
     </Modal>
