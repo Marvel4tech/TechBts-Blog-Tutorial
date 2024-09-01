b33684ef7b944efd9151d62eb79a56031a629d9a
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-09-01 12:53:30 +0100

    commit

--- src/components/common/Comments/Comments.jsx
+++ src/components/common/Comments/Comments.jsx
@@ -6,8 +6,13 @@ const Comments = () => {
 
   return (
     <Modal setModal={showModal} modal={showModal}>
-        <section className={` fixed right-0 bottom-0 top `}>
-
+        <section className={` fixed right-0 bottom-0 top-0 border border-gray-300 z-50 bg-white w-[22rem] shadow-2xl p-5 overflow-y-auto transition-all 
+        duration-500`}>
+            <div>
+                <h3>
+                    Responses(1)
+                </h3>
+            </div>
         </section>
     </Modal>
   )
