1af8bfb09942ca9560e4b08e7de707aff757bf0d
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-09-01 01:38:05 +0100

    commit

--- src/components/common/Posts/Actions/Like.jsx
+++ src/components/common/Posts/Actions/Like.jsx
@@ -1,18 +1,28 @@
 import { PiHandsClappingDuotone } from 'react-icons/pi'
 import { Blog } from '../../../../context/Context'
-import { doc } from '@firebase/firestore'
+import { deleteDoc, doc, setDoc } from '@firebase/firestore'
 import { db } from '../../../../firebaseConfig/firebase'
+import { useState } from 'react'
+import { toast } from 'react-toastify'
 
-const Like = ({ post }) => {
+const Like = ({ post, postId }) => {
+  const [isLiked, setIsLiked] = useState(false)
   const { currentUser } = Blog()
 
   const handleLike = async () => {
     try {
       if (currentUser) {
-        const likeRef = doc(db, "posts", post.id)
+        const likeRef = doc(db, "posts", postId, "likes", currentUser?.uid)
+        if(isLiked) {
+          await deleteDoc(likeRef)
+        } else {
+          await setDoc(likeRef, {
+            userId: currentUser?.uid
+          })
+        }
       }
     } catch (error) {
-      
+      toast.error(error.message)
     }
   }
 
