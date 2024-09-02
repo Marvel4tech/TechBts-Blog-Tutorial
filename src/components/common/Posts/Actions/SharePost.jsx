6fc587e5d4215cce11d6a7cd9f64b5e222d24498
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-09-02 18:21:03 +0100

    share and copy link is implemented / added

--- src/components/common/Posts/Actions/SharePost.jsx
+++ src/components/common/Posts/Actions/SharePost.jsx
@@ -8,13 +8,15 @@ import { toast } from 'react-toastify'
 const SharePost = () => {
   const [showDrop, setShowDrop] = useState(false)
 
-  const path = window.location.path
+  const path = window.location.href
   const copyLink = async () => {
     try {
         await navigator.clipboard.writeText(path)
         toast.success("Link has been copied")
+        setShowDrop(false)
     } catch (error) {
         toast.error(error.message)
+        setShowDrop(false)
     }
   }
 
@@ -24,10 +26,16 @@ const SharePost = () => {
               <CiShare1 className=' text-2xl'/>
           </button>
           <DropDown showDrop={showDrop} setShowDrop={setShowDrop} size="w-[12rem]" className=" border shadow-xl" >
-              <Button click={""} title="Copy Link" icon={<BiLink />} />
-              <Button click={""} title="Share on Twitter" icon={<BiLogoTwitter />} />
-              <Button click={""} title="Share on FaceBook" icon={<BiLogoFacebookCircle />} />
-              <Button click={""} title="Share on LinkedIn" icon={<BiLogoLinkedinSquare />} />
+              <Button click={copyLink} title="Copy Link" icon={<BiLink />} />
+              <TwitterShareButton url={path} >
+                    <Button title="Share on Twitter" icon={<BiLogoTwitter />} isChild={true}/>
+              </TwitterShareButton>
+              <FacebookShareButton url={path} >
+                    <Button title="Share on FaceBook" icon={<BiLogoFacebookCircle />} isChild={true} />
+              </FacebookShareButton>
+              <LinkedinShareButton url={path} >
+                    <Button title="Share on LinkedIn" icon={<BiLogoLinkedinSquare />} isChild={true} />
+              </LinkedinShareButton>
           </DropDown>
       </div>
   )
@@ -36,12 +44,24 @@ const SharePost = () => {
 export default SharePost
 
 
-const Button = ({ click, title, icon }) => {
-    return (
-        <button className=' p-2 hover:bg-gray-200 hover:text-black/80 w-full text-sm text-left flex items-center gap-2 cursor-pointer
-        text-gray-500'>
-            <span className=' text-[1.2rem]'>{icon}</span>
-            {title}
-        </button>
-    )
-}
+const Button = ({ click, title, icon, children, isChild }) => {
+    if (isChild) {
+        return (
+            <span className=' p-2 hover:bg-gray-200 hover:text-black/80 w-full text-sm text-left flex items-center gap-2 cursor-pointer
+            text-gray-500'>
+                <span className=' text-[1.2rem]'>{icon}</span>
+                {title}
+                {children}
+            </span>
+        )
+    } else {
+        return (
+            <button className=' p-2 hover:bg-gray-200 hover:text-black/80 w-full text-sm text-left flex items-center gap-2 cursor-pointer
+            text-gray-500' onClick={click}>
+                <span className=' text-[1.2rem]'>{icon}</span>
+                {title}
+                {children}
+            </button>
+        );
+    }
+};
