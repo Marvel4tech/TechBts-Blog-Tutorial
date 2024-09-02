37b9e8725519c6d6f419848e2e3fe75bf8656e96
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-09-02 18:37:05 +0100

    commit

--- src/components/common/Posts/Actions/Actions.jsx
+++ src/components/common/Posts/Actions/Actions.jsx
@@ -18,7 +18,7 @@ const Actions = ({ postId }) => {
             <BsThreeDots className=' text-2xl' />
         </button>
         <DropDown showDrop={showDrop} setShowDrop={setShowDrop} size="w-[7rem]">
-            <Button onclick={() => navigate(`/editPost/${postId}`)} title="Edit Story" />
+            <Button click={() => navigate(`/editPost/${postId}`)} title="Edit Story" />
             <Button onclick="" title="Delete Story" />
         </DropDown>
     </div>
@@ -30,7 +30,7 @@ export default Actions
 
 const Button = ({ click, title }) => {
     return (
-        <button className={` p-2 hover:bg-gray-100 hover:text-black/80 w-full text-sm text-left 
+        <button onClick={click} className={` p-2 hover:bg-gray-100 hover:text-black/80 w-full text-sm text-left 
           ${title === "Delete Story" ? "text-red-600" : ""}`}>
             {title}
         </button>
