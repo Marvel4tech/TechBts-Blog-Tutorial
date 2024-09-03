c30b7e0f91f243099d425972491ccc2facc3ab96
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-09-03 02:32:04 +0100

    commit

--- src/components/common/Posts/EditPost.jsx
+++ src/components/common/Posts/EditPost.jsx
@@ -7,8 +7,20 @@ const EditPost = () => {
 
   return (
     <section className=' w-[90%] md:w-[80%] lg:w-[60%] mx-auto py-[3rem]'>
-        <input type="text" placeholder=' Title...' className=' text-4xl outline-none w-full' />
-        <ReactQuill placeholder='Description...' className=' !text-[4rem] my-3' theme='bubble' />
+        <input 
+            type="text" 
+            placeholder=' Title...' 
+            className=' text-4xl outline-none w-full' 
+            value={title}
+            onChange={() => setTitle (e.target.value)}
+        />
+        <ReactQuill 
+            placeholder='Description...' 
+            className=' !text-[4rem] my-3' 
+            theme='bubble' 
+            value={description}
+            onChange={setDescription}
+        />
     </section>
   )
 }
