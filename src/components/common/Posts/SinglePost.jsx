76a83968825733e6019208a373d24508a4fc0270
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-09-03 13:42:57 +0100

    Trending Post starts

--- src/components/common/Posts/SinglePost.jsx
+++ src/components/common/Posts/SinglePost.jsx
@@ -1,5 +1,5 @@
 import { doc, getDoc } from '@firebase/firestore'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { db } from '../../../firebaseConfig/firebase'
 import { toast } from 'react-toastify'
@@ -20,6 +20,22 @@ const SinglePost = () => {
     const [post, setPost] = useState({})
     const [loading, setLoading] = useState(false)
     const { currentUser } = Blog()
+
+    //increment page views
+    const isInitialRender = useRef(true)
+    useEffect(() => {
+        if(isInitialRender?.current){
+            const incrementPageView = async () => {
+                try {
+                    
+                } catch (error) {
+                    
+                }
+            }
+
+            incrementPageView()
+        }
+    }, [])
 
     useEffect(() => {
         const fetchPost = async () => {
