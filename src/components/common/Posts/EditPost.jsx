27edb4ec4f39c6b07ea696e07f8925f034305613
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-09-03 02:28:49 +0100

    commit

--- src/components/common/Posts/EditPost.jsx
+++ src/components/common/Posts/EditPost.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactQuill from 'react-quill'
 
 const EditPost = () => {
+    const [title, setTitle] = useState("")
+    const [description, setDescription] = useState("")
+
   return (
     <section className=' w-[90%] md:w-[80%] lg:w-[60%] mx-auto py-[3rem]'>
         <input type="text" placeholder=' Title...' className=' text-4xl outline-none w-full' />
